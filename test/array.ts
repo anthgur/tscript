6c7d536fb608f5bef7bbec3d4bec9a91d1d81164
Anthony Urena <anthgur@users.noreply.github.com>
2015-04-27 18:33:16 -0400

    Added some bullshit for arrays

--- test/array.ts
+++ test/array.ts
@@ -1,16 +1,63 @@
-Array = new 42;
-
-Array.push = function(a, x) {
-  print (a.length);
-  a.length = 42;
-  print (a.length);
-  //if (n == undefined) {
-  //  n = 0;
-  //}
-  //a[n] = 42;
-  //a.length = n + 1;
+Array = function() {
+  var a = new "";
+  a.length = 0;
+  return a;
 };
 
-var a = new 42;
-//a.lol = "lol";
-Array.push(a, 42);
+Array.push = function(a, x) {
+  var n = a.length;
+  if (n == undefined) {
+    n = 0;
+  }
+  a[n] = x;
+  a.length = n + 1;
+};
+
+Array.pop = function(a) {
+  var n = a.length - 1;
+  if (n == undefined) {
+    return;
+  }
+  a[n] = undefined;
+  a.length = n;
+};
+
+Array.map = function(a, f) {
+  var b = new 2
+    , n = a.length
+    , x = 0;
+  while (x < n) {
+    b[x] = f(a[x]);
+    x = x + 1;
+  }
+  return b;
+};
+
+var startProd, startSymbol, productions;
+productions = Array();
+
+startProd = readln();
+Array.push(productions, startProd);
+
+while(true) {
+  var line = readln();
+  if (line == "") {
+    break;
+  }
+  Array.push(productions, line);
+}
+
+// test code here
+var test = function() {
+  var a = Array();
+  Array.push(a, 42);
+  Array.pop(a);
+
+  var b = Array();
+  Array.push(b, 1);
+  Array.push(b, 2);
+  Array.push(b, 3);
+  var c = Array.map(b, function(x) { print(x);});
+};
+
+//test();
